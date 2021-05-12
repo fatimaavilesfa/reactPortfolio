afd7b6e55a2ea39c90b2ae1db045f105caf95517
fatimaavilesfa <fatimaaviles1612@gmial.com>
2021-05-11 21:25:38 -0600

    Updates

---
+++ precache-manifest.3ab1b36b2e5082f9dc8550b8800b3126.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9162b07675be2859d60f670c30c03e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b081ab3a5ac7bff0e3b",
+    "url": "/static/css/main.13f462dc.chunk.css"
+  },
+  {
+    "revision": "e1742731a298abaddaba",
+    "url": "/static/js/2.e8e33f9e.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.e8e33f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b081ab3a5ac7bff0e3b",
+    "url": "/static/js/main.fa23d804.chunk.js"
+  },
+  {
+    "revision": "ad8caf16babc2df44569",
+    "url": "/static/js/runtime-main.e65a3091.js"
+  },
+  {
+    "revision": "08043b877c7bbd226b6e369b5c663f16",
+    "url": "/static/media/aw1.08043b87.png"
+  },
+  {
+    "revision": "aabfac02d0913624a1dfba67e794b5c6",
+    "url": "/static/media/gallery.aabfac02.png"
+  },
+  {
+    "revision": "e73c5719d29c1d0cd7b289072a28ac14",
+    "url": "/static/media/ghibli.e73c5719.png"
+  },
+  {
+    "revision": "68791ff08fef80c632bd0d0d9231752b",
+    "url": "/static/media/purple2.68791ff0.jpg"
+  },
+  {
+    "revision": "f61292665d6196b35196641af111adc6",
+    "url": "/static/media/weatherApp.f6129266.png"
+  }
+]);
